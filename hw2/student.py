a1c2dedceaf149da1439952839d4edc8dc6f01db
Shakeel Anver <shakeelanver26@gmail.com>
2021-11-09 13:48:56 +1100

    convnet works better, needs optimizing

--- hw2/student.py
+++ hw2/student.py
@@ -73,13 +73,38 @@ class DenseNet(nn.Module):
         # print(f"Output Shape: {self.output.shape}")
         return self.output
 
+class ConvNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_layer_one = nn.Conv2d(in_channels=3, out_channels=20, kernel_size=(5,5))
+        self.max_pool_one = nn.MaxPool2d(kernel_size=(2,2),stride=(2,2))
+        self.conv_layer_two = nn.Conv2d(in_channels=20, out_channels=70, kernel_size=(5,5))
+        self.max_pool_two = nn.MaxPool2d(kernel_size=(2,2),stride=(2,2))
+        self.fc1 = nn.Linear(in_features=17*17*70,out_features=10)
+        
+    def forward(self, input):
+        # print(f"input shape as input: {input.shape}")
+        # input = input.view(200,-1)
+        input = F.relu(self.conv_layer_one(input))
+        # print(f"input shape after first conv layer: {input.shape}")
+        input = self.max_pool_one(input)
+        # print(f"input shape after max pool one: {input.shape}")
+        input = F.relu(self.conv_layer_two(input))
+        # print(f"input shape after second conv layer: {input.shape}")
+        input = self.max_pool_two(input)
+        # print(f"input shape after second max pooling: {input.shape}")
+        input = input.view(-1,17*17*70)
+        input = F.log_softmax(self.fc1(input),dim=1)
+        return input # CHANGE CODE HERE       
+
 # net = Network()
-net = DenseNet(50)
+# net = DenseNet(50)
+net = ConvNet()
     
 ############################################################################
 ######      Specify the optimizer and loss function                   ######
 ############################################################################
-optimizer = optim.Adam(net.parameters(),lr=0.005)
+optimizer = optim.Adam(net.parameters(),lr=0.001, betas=(0.9,0.999))
 
 loss_func = nn.CrossEntropyLoss()
 
