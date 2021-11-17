193cf3d1fd8f6f7a41b04edcbb0f857176cb3b97
Oltan Sevinc <m.sevinc@student.unsw.edu.au>
2021-11-17 08:46:50 +0300

    forgot to remove 2 references to expansion

--- hw2/student.py
+++ hw2/student.py
@@ -55,8 +55,7 @@ def transform(mode):
     if mode == 'train':
         tf = transforms.Compose([
             transforms.RandomAffine(degrees=30, translate=(0.1,0.2), scale=(0.5, 1)),
-            transforms.RandomHorizontalFlip(),
-            
+            transforms.RandomHorizontalFlip(),        
             transforms.RandomAdjustSharpness(sharpness_factor=2),
             transforms.RandomPerspective(),
             transforms.RandomVerticalFlip(),
@@ -75,8 +74,8 @@ class Layer(nn.Module):
         super(Layer, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0)
-        self.bn2 = nn.BatchNorm2d(out_channels * self.expansion)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
         self.identity_downsample = identity_downsample
 
